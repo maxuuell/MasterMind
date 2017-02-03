780e14a204f43302a7405e3e189938eec7e60d58
SherryH <enjoy.wind7rain@gmail.com>
2017-02-03 15:49:55 -0800

    Able to dynamically populate table columns from the array

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -7,7 +7,7 @@ export class Profile extends React.Component {
     this.state = {
       username: "",
       highScoreMem: null,
-      highscoreScram: null,
+      highScoreScram: null,
       memScores:[],
       scramScores:[]
     };
@@ -35,30 +35,107 @@ export class Profile extends React.Component {
 
     //assume we get the user info from the GET
     var userInfo = {
-      username: "cloud",
+      username: 'cloud',
       highScoreMem: 100,
-      highscoreScram: 101,
-      memScores: [100, 80],
-      scramScores: [20, 20]
+      highScoreScram: 101,
+      memScores: [100, 200],
+      scramScores: [101]
     };
+
+    // var gamesPlayed = ()=>{
+    //   var games =[];
+    //   if (userInfo.memScores.length !== 0) {
+    //     games.push('');
+    //   }
+
+    // };
+
     this.setState({
       username: userInfo.username,
       highScoreMem: userInfo.highScoreMem,
       highScoreScram: userInfo.highScoreScram,
-      memScores:userInfo.memScores,
-      scramScores:userInfo.scramScores
+      memScores: userInfo.memScores,
+      scramScores: userInfo.scramScores
     });
   }
 
 
   scoreDisplay () {
-    var OneGameScoreDisplay = ()=>(
+    //---- Element for displaying one game ----
+    var OneGameScoreDisplay = ({gameType, score, scoreArr})=>(
       <div>
-        <h2 className="text-center">Highest Memory Game Score: <span className="highscore">{this.state.highScoreMem}</span></h2>
+        <h2 className="text-center">Highest {gameType} Game Score: <span className="highscore">{score}</span></h2>
+
+        <div className="table-responsive">
+          <table className="table">
+            <tbody>
+              <tr>
+                <th>#</th>
+                {
+                  scoreArr.map((eachScore, ind)=>{
+                    return <TableCol value={ind + 1} key={ind}/>;
+                  })
+                }
+              </tr>
+              <tr>
+                <th>Score</th>
+                {
+                  scoreArr.map((eachScore, ind)=>{
+                    return <TableCol value={eachScore} key={eachScore}/>;
+                  })
+                }
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     );
+
+    var TableCol = ({value}) =>(
+      <td>{value}</td>
+    );
+
+    var OneGameScoreArrayDisplay = ({score, ind})=>{
+      console.log('Inside GameArray Display score:', score, 'ind:',ind);
+      return(
+        <div className="table-responsive">
+          <table className="table">
+            <tbody>
+              <tr>
+                <th>#</th>
+                <td>{ind}</td>
+              </tr>
+              <tr>
+                <th>Score</th>
+                <td>{score}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      );
+    };
+    //------------------------------------
+    //---- Logics for displaying game elem--
+    //--- Potentially refactor for the games to reside in one array for easy data manipulation
+    let gameDisplay = [];
+    if (this.state.memScores.length !== 0) {
+      gameDisplay.push(<OneGameScoreDisplay gameType='Memory' score={this.state.highScoreMem} scoreArr={this.state.memScores} key='Memory' />);
+
+    }
+    if (this.state.scramScores.length !== 0) {
+      gameDisplay.push(<OneGameScoreDisplay gameType='Scramble' score={this.state.highScoreScram} scoreArr={this.state.scramScores} key='Scramble' />);
+    }
+
     return (
-      <OneGameScoreDisplay />
+      <div>
+        {gameDisplay}
+        {this.state.memScores.map((score, ind)=>{
+          console.log('score:', score.toString(), 'ind:', ind);
+
+          return <OneGameScoreArrayDisplay score={score} ind={ind} key={ind.toString()} />;
+        })}
+
+      </div>
     );
   }
 
@@ -71,7 +148,7 @@ export class Profile extends React.Component {
     );
     //--------------------------------------------
 
-    {console.log('inside displayProfile',this.state.memScores.length)}
+    {console.log('inside displayProfile',this.state.memScores.length);}
     //the logics to decide what elements to display
     if (this.state.memScores.length === 0 && this.state.memScores.length === 0) {
       profileElem = (NoScoreDisplay);
