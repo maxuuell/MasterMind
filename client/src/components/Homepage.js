8b385cd20674d0e9a4d0d633932f669ffe0fb4d0
Michael Mills <rizz1337@gmail.com>
2017-02-06 10:27:17 -0800

    Update homepage.html before pull

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -4,7 +4,8 @@ export default class Homepage extends React.Component {
   render() {
     return (
       <div>
-        <h2>Homepage2</h2>
+        <h2>Welcome to MasterMind!</h2>
+        <h4>MasterMind is a collection of games designed to test and strengthen your mental fortitude. Anyone is allowed to play these games, but creating an account allows you the ability to keep track of your personal progress.</h4>
       </div>
     );
   }
