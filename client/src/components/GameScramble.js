e3133d79bde60797564bcf1f3a53dcf9f85701fd
Michael Mills <rizz1337@gmail.com>
2017-02-06 12:31:45 -0800

    Changed timer time

--- client/src/components/GameScramble.js
+++ client/src/components/GameScramble.js
@@ -20,7 +20,7 @@ export default class GameScramble extends React.Component {
       definition: null,
       shuffled: null,
       score: 0,
-      timeLeft: 20
+      timeLeft: 30
     };
     //send a GET for random word
     var context = this;
