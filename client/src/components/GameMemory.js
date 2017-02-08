d4fb75d13d3023b9300a5b61306d0ac6dce97837
Jimmie Gisclair <jimmiegisclair@gmail.com>
2017-02-08 15:22:00 -0800

    Set up a basic Phaser skeleton in GameMemory.js

--- client/src/components/GameMemory.js
+++ client/src/components/GameMemory.js
@@ -4,8 +4,30 @@ export default class GameMemory extends React.Component {
   render() {
     return (
       <div>
-        <h2>Memory Game here</h2>
+        <h2>Jimmie's Phaser Game!</h2>
+        <div id="phaser-game"></div>
       </div>
     );
   }
+
+  componentDidMount() {
+    this.createGame();
+  }
+
+  createGame() {
+    var game = new Phaser.Game(400, 250, Phaser.AUTO, 'phaser-game', {preload: preload, create: create, update});
+
+    function preload() {
+
+    }
+
+    function create() {
+
+    }
+
+    function update() {
+
+    }
+  }
+  
 }
