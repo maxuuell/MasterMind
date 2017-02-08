8b7c42cebc19516a612e1053655c70e66fb0df55
Cory Wolnewitz <wolnewitz@gmail.com>
2017-02-07 20:19:56 -0800

    Remove Link from index

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { IndexRoute, Router, Route, Link, hashHistory } from 'react-router';
+import { IndexRoute, Router, Route, hashHistory } from 'react-router';
 
 import App from './components/App';
 import SignUp from './components/SignUp';
