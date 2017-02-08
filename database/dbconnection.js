84bfd072d97f465c0f59c3829b87397284e85136
maxuuell <maxuuell@gmail.com>
2017-02-08 14:18:24 -0800

    Working on testing db.

--- database/dbconnection.js
+++ database/dbconnection.js
@@ -1,2 +1,23 @@
+var mongoose = require("mongoose");
 var models = require("./models.js");
 
+// to establish mLabUri for Heroku's cloud hosted mongo db
+var mLabUri = 'mongodb://' + process.env.DBUSER + ':' + process.env.DBPASS + '@ds137749.mlab.com:37749/megaminds';
+
+// to establish mongo db connection on localhost.
+var localMongoUri = 'mongodb://localhost/megaminds';
+
+// to determine which Uri to use, based on wheterh production or local.
+var MONGO_URI = (process.env.NODE_ENV === 'production') ? mLabUri : localMongoUri;
+
+mongoose.connect(MONGO_URI);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection to mongoose error:'));
+
+db.once('open', function() {
+  console.log('we connected to mongoose!');
+});
+
+module.exports = db;
--- server/server.js
+++ server/server.js
@@ -1,42 +1,43 @@
 var express = require('express');
 var bodyparser = require('body-parser');
 var mongoose = require('mongoose');
+var db = require("../database/dbconnection.js");
 var path = require('path');
-var userController = require('./users/userController.js');
-var session = require('express-session');
-var config = require('./config.js');
+// var userController = require('./users/userController.js');
+// var session = require('express-session');
+// var config = require('./config.js');
 var app = express();
 
 //middlewares
 app.use(bodyparser.json());
-app.use(session({
-  user: null,
-  secret: 'master of my domain',
-  cookie: {maxAge: 31536000000}
-}));
+// app.use(session({
+//   user: null,
+//   secret: 'master of my domain',
+//   cookie: {maxAge: 31536000000}
+// }));
 
-//require login
-var requireLogin = function(req, res, next) {
-  req.session.user ? res.render('/profile') : res.redirect('/login');
-};
+// //require login
+// var requireLogin = function(req, res, next) {
+//   req.session.user ? res.render('/profile') : res.redirect('/login');
+// };
 
-//to remove the mongoose Promise deprecated warning
-mongoose.Promise = global.Promise;
+// //to remove the mongoose Promise deprecated warning
+// mongoose.Promise = global.Promise;
 
-//Mongoose
-// var uri;
-// process.env.PORT ? uri = config.web : uri = config.local;
-var mLabUri = 'mongodb://' + process.env.DBUSER + ':' + process.env.DBPASS + '@ds137749.mlab.com:37749/mastermind';
-var localMongoUri = 'mongodb://localhost/mastermind';
-var MONGO_URI = (process.env.NODE_ENV === 'production') ? mLabUri : localMongoUri;
-mongoose.connect(MONGO_URI);
+// //Mongoose
+// // var uri;
+// // process.env.PORT ? uri = config.web : uri = config.local;
+// var mLabUri = 'mongodb://' + process.env.DBUSER + ':' + process.env.DBPASS + '@ds137749.mlab.com:37749/mastermind';
+// var localMongoUri = 'mongodb://localhost/mastermind';
+// var MONGO_URI = (process.env.NODE_ENV === 'production') ? mLabUri : localMongoUri;
+// mongoose.connect(MONGO_URI);
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection to mongoose error:'));
-db.once('open', function() {
-  console.log('we connected to mongoose!');
-  console.log('mLabUri', mLabUri);
-});
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection to mongoose error:'));
+// db.once('open', function() {
+//   console.log('we connected to mongoose!');
+//   console.log('mLabUri', mLabUri);
+// });
 
 
 //define port
@@ -63,14 +64,14 @@ app.get('/memory', function(req, res) {
 app.get('/scramble', function(req, res) {
   res.send('This is where we would serve the scramble game');
 });
-app.get('/:username', userController.getUser);
-app.get('/leaderboard', userController.getAll);
+// app.get('/:username', userController.getUser);
+// app.get('/leaderboard', userController.getAll);
 
-//post routes
-app.post('/signup', userController.signup);
-app.post('/login', userController.login);
-app.post('/scores', userController.postScore);
-app.post('/logout', userController.logout);
+// //post routes
+// app.post('/signup', userController.signup);
+// app.post('/login', userController.login);
+// app.post('/scores', userController.postScore);
+// app.post('/logout', userController.logout);
 
 
 app.listen(port, function () {
