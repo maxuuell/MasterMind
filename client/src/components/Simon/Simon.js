4852242be2ee9bae3e96f9443d7eaff2f33da15f
Renata Maraj <renatavmaraj@gmail.com>
2017-02-11 10:25:56 -0800

    Phaser Simon

--- client/src/components/Simon/Simon.js
+++ client/src/components/Simon/Simon.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
 export default class Simon extends React.Component {
-render() {
-return (
-  <div>
-    <div id="simon"></div>
-  </div>
-);
-}
+  render() {
+    return (
+      <div>
+        <div id="simon"></div>
+      </div>
+    );
+  }
 
-componentDidMount() {
-this.createGame();
-}
+  componentDidMount() {
+    this.createGame();
+  }
 
-createGame() {
+  createGame() {
     var game = new Phaser.Game(740, 480, Phaser.AUTO, 'simon', { preload: preload, create: create, update: update });
 
     var simon;
@@ -29,181 +29,176 @@ createGame() {
     var loser;
     var intro;
 
-    function preload() {
-        game.load.image('background', 'client/src/components/Semory/imgs/blue.jpg');
-        game.load.spritesheet('item', 'client/src/components/Semory/imgs/number-buttons', 160, 160);
+    preload() {
+      game.load.image('background', 'client/src/components/Semory/imgs/blue.jpg');
+      game.load.spritesheet('item', 'client/src/components/Semory/imgs/number-buttons', 160, 160);
     }
 
-              function update() {
+    update() {
 
-                if (simonSez) {
-                    if (game.time.now - timeCheck > 700 - N * 40) {
-                        simon.getAt(litSquare).alpha = .25;
-                        game.paused = true;
+      if (simonSez) {
+        if (game.time.now - timeCheck > 700 - N * 40) {
+        simon.getAt(litSquare).alpha = .25;
+        game.paused = true;
 
-                        setTimeout(function() {
-                            if (currentCount < N) {
-                                game.paused = false;
-                                simonSequence();
-                            } else {
-                                simonSez = false;
-                                game.paused = false;
-                            }
-                        }, 400 - N * 20);
-                    }
-                }
+          setTimeout(function() {
+            if (currentCount < N) {
+              game.paused = false;
+              simonSequence();
+            } else {
+              simonSez = false;
+              game.paused = false;
             }
+          }, 400 - N * 20);
+        }
+      }
+    }
 
-    function create() {
+    create() {
 
-            simon = game.add.group();
-            var item;
+      simon = game.add.group();
+      var item;
 
-            for (var i = 0; i < 3; i++) {
-                item = simon.create(150 + 168 * i, 150, 'item', i);
-                // Enable input.
-                item.inputEnabled = true;
-                item.input.start(0, true);
-                item.events.onInputDown.add(select);
-                item.events.onInputUp.add(release);
-                item.events.onInputOut.add(moveOff);
-                simon.getAt(i).alpha = 0;
-            }
+      for (var i = 0; i < 3; i++) {
+        item = simon.create(150 + 168 * i, 150, 'item', i);
+        // Enable input.
+        item.inputEnabled = true;
+        item.input.start(0, true);
+        item.events.onInputDown.add(select);
+        item.events.onInputUp.add(release);
+        item.events.onInputOut.add(moveOff);
+        simon.getAt(i).alpha = 0;
+      }
 
-            for (var i = 0; i < 3; i++) {
-                item = simon.create(150 + 168 * i, 318, 'item', i + 3);
-                // Enable input.
-                item.inputEnabled = true;
-                item.input.start(0, true);
-                item.events.onInputDown.add(select);
-                item.events.onInputUp.add(release);
-                item.events.onInputOut.add(moveOff);
-                simon.getAt(i + 3).alpha = 0;
-            }
+    for (var i = 0; i < 3; i++) {
+        item = simon.create(150 + 168 * i, 318, 'item', i + 3);
+        // Enable input.
+        item.inputEnabled = true;
+        item.input.start(0, true);
+        item.events.onInputDown.add(select);
+        item.events.onInputUp.add(release);
+        item.events.onInputOut.add(moveOff);
+        simon.getAt(i + 3).alpha = 0;
+    }
 
-            introTween();
-            setUp();
-            setTimeout(function() { simonSequence();
-                intro = false; }, 6000);
+    introTween();
+    setUp();
+    setTimeout(function() { simonSequence();
+      intro = false; }, 6000);
 
 
-            function restart() {
+    restart() {
+      N = 1;
+      userCount = 0;
+      currentCount = 0;
+      sequenceList = [];
+      winner = false;
+      loser = false;
+      introTween();
+      setUp();
+      setTimeout(function() { simonSequence();
+      intro = false; }, 6000);
+    }
 
-                N = 1;
-                userCount = 0;
-                currentCount = 0;
-                sequenceList = [];
-                winner = false;
-                loser = false;
-                introTween();
-                setUp();
-                setTimeout(function() { simonSequence();
-                    intro = false; }, 6000);
+    introTween() {
 
-            }
+      intro = true;
 
-            function introTween() {
+      for (var i = 0; i < 6; i++) {
+        var flashing = game.add.tween(simon.getAt(i)).to({ alpha: 1 }, 500, Phaser.Easing.Linear.None, true, 0, 4, true);
+        var final = game.add.tween(simon.getAt(i)).to({ alpha: .25 }, 500, Phaser.Easing.Linear.None, true);
 
-                intro = true;
-
-                for (var i = 0; i < 6; i++) {
-                    var flashing = game.add.tween(simon.getAt(i)).to({ alpha: 1 }, 500, Phaser.Easing.Linear.None, true, 0, 4, true);
-                    var final = game.add.tween(simon.getAt(i)).to({ alpha: .25 }, 500, Phaser.Easing.Linear.None, true);
-
-                    flashing.chain(final);
-                    flashing.start();
-                }
-
-            }
-
-            function playerSequence(selected) {
-
-                correctSquare = sequenceList[userCount];
-                userCount++;
-                thisSquare = simon.getIndex(selected);
-
-                if (thisSquare == correctSquare) {
-                    if (userCount == N) {
-                        if (N == sequenceCount) {
-                            winner = true;
-                            setTimeout(function() { restart(); }, 3000);
-                        } else {
-                            userCount = 0;
-                            currentCount = 0;
-                            N++;
-                            simonSez = true;
-                        }
-                    }
-                } else {
-                    loser = true;
-                    setTimeout(function() { restart(); }, 3000);
-                }
-
-            }
-
-            function simonSequence() {
-
-                simonSez = true;
-                litSquare = sequenceList[currentCount];
-                simon.getAt(litSquare).alpha = 1;
-                timeCheck = game.time.now;
-                currentCount++;
-
-            }
-
-            function setUp() {
-
-                for (var i = 0; i < sequenceCount; i++) {
-                    thisSquare = game.rnd.integerInRange(0, 5);
-                    sequenceList.push(thisSquare);
-                }
-
-            }
-
-            function select(item, pointer) {
-
-                if (!simonSez && !intro && !loser && !winner) {
-                    item.alpha = 1;
-                }
-
-            }
-
-            function release(item, pointer) {
-
-                if (!simonSez && !intro && !loser && !winner) {
-                    item.alpha = .25;
-                    playerSequence(item);
-                }
-            }
-
-            function moveOff(item, pointer) {
-
-                if (!simonSez && !intro && !loser && !winner) {
-                    item.alpha = .25;
-                }
-
-            }
-
-            function render() {
-
-                if (!intro) {
-                    if (simonSez) {
-                        game.debug.text('Simon Sez', 360, 96, 'rgb(255,0,0)');
-                    } else {
-                        game.debug.text('Your Turn', 360, 96, 'rgb(0,255,0)');
-                    }
-                } else {
-                    game.debug.text('Get Ready', 360, 96, 'rgb(0,0,255)');
-                }
-
-                if (winner) {
-                    game.debug.text('You Win!', 360, 32, 'rgb(0,0,255)');
-                } else if (loser) {
-                    game.debug.text('You Lose!', 360, 32, 'rgb(0,0,255)');
-                }
-
-            }
+        flashing.chain(final);
+        flashing.start();
+      }
 
     }
+
+    playerSequence(selected) {
+
+      correctSquare = sequenceList[userCount];
+      userCount++;
+      thisSquare = simon.getIndex(selected);
+
+      if (thisSquare == correctSquare) {
+        if (userCount == N) {
+          if (N == sequenceCount) {
+            winner = true;
+            setTimeout(function() { restart(); }, 3000);
+            }
+          else {
+            userCount = 0;
+            currentCount = 0;
+            N++;
+            simonSez = true;
+          }
+        }
+      } else {
+          loser = true;
+          setTimeout(function() { restart(); }, 3000);
+      }
+
+    }
+
+    simonSequence() {
+
+      simonSez = true;
+      litSquare = sequenceList[currentCount];
+      simon.getAt(litSquare).alpha = 1;
+      timeCheck = game.time.now;
+      currentCount++;
+
+    }
+
+    setUp() {
+
+      for (var i = 0; i < sequenceCount; i++) {
+        thisSquare = game.rnd.integerInRange(0, 5);
+        sequenceList.push(thisSquare);
+      }
+
+    }
+
+    select(item, pointer) {
+      if (!simonSez && !intro && !loser && !winner) {
+        item.alpha = 1;
+      }
+    }
+
+    release(item, pointer) {
+
+      if (!simonSez && !intro && !loser && !winner) {
+        item.alpha = .25;
+        playerSequence(item);
+      }
+    }
+
+    moveOff(item, pointer) {
+      if (!simonSez && !intro && !loser && !winner) {
+        item.alpha = .25;
+      }
+    }
+
+    render() {
+
+      if (!intro) {
+        if (simonSez) {
+        game.debug.text('Simon Sez', 360, 96, 'rgb(255,0,0)');
+        } else {
+        game.debug.text('Your Turn', 360, 96, 'rgb(0,255,0)');
+        }
+      } else {
+        game.debug.text('Get Ready', 360, 96, 'rgb(0,0,255)');
+      }
+
+      if (winner) {
+        game.debug.text('You Win!', 360, 32, 'rgb(0,0,255)');
+      } else if (loser) {
+        game.debug.text('You Lose!', 360, 32, 'rgb(0,0,255)');
+      }
+
+    }
+
+  }
 }
 }
