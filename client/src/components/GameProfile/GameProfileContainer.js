1ab12f918add10c51c2584f13be2db258707d039
Cory Wolnewitz <wolnewitz@gmail.com>
2017-02-11 12:50:45 -0800

    Add pagination bar

--- client/src/components/GameProfile/GameProfileContainer.js
+++ client/src/components/GameProfile/GameProfileContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Pagination } from 'react-bootstrap';
 
 export default class GameProfileContainer extends Component {
   constructor(props) {
@@ -6,18 +7,34 @@ export default class GameProfileContainer extends Component {
     this.state = {
       loadingData: false,
       filteredGames: [],
-      gameName: this.props.location.pathname.split('/')[2]
+      gameName: this.props.location.pathname.split('/')[2],
+      activePage: 1
     }
     this.getHeader = this.getHeader.bind(this)
+    this.handleSelect = this.handleSelect.bind(this)
   }
 
   getHeader() {
     return `${this.props.profile.given_name}'s ${this.state.gameName} Stats`;
   }
 
+  handleSelect(eventKey) {
+    this.setState({
+      activePage: eventKey
+    })
+  }
+
   render() {
     return (
-      <h1 className='text-center'>{this.getHeader()}</h1>
+      <div className='text-center'>
+        <h1 className='text-center'>{this.getHeader()}</h1>
+        <Pagination
+          bsSize='medium'
+          items={10}
+          activePage={this.state.activePage}
+          onSelect={this.handleSelect}
+        />
+      </div>
     )
   }
 }
