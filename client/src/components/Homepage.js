5ad68c34a6fe5ac13ccf808e750deb141aca8a02
Jimmie Gisclair <jimmiegisclair@gmail.com>
2017-02-10 18:48:34 -0800

    Remove the large pictures from homepage

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -13,49 +13,6 @@ export default class Homepage extends React.Component {
             you the ability to keep track of your personal progress</h4>
       </div>
     </div>
-    <div className="row mt centered">
-      <div className="col-lg-4">
-        <a className="zoom green" href="work01.html"><img className="img-responsive" src="assets/img/portfolio/port01.jpg" alt="" /></a>
-        <p>APE</p>
-        <div>
-        <Link to="/memorygame"> Memory</Link>
-        </div>
-        <div>
-        <Link to="/scramblegame"> Scramble</Link>
-        </div>
-        <div>
-        <Link to="/simon">Simon</Link>
-        </div>
-
-      </div>
-      <div className="col-lg-4">
-        <a className="zoom green" href="work01.html"><img className="img-responsive" src="assets/img/portfolio/port02.jpg" alt="" /></a>
-        <p>RAIDERS</p>
-      </div>
-      <div className="col-lg-4">
-        <a className="zoom green" href="work01.html"><img className="img-responsive" src="assets/img/portfolio/port03.jpg" alt="" /></a>
-        <p>VIKINGS</p>
-      </div>
-    </div>
-
-
-    <div className="row mt centered">
-
-      <div className="col-lg-4">
-        <a className="zoom green" href="work01.html"><img className="img-responsive" src="assets/img/portfolio/port04.jpg" alt="" /></a>
-        <p>YODA</p>
-      </div>
-
-      <div className="col-lg-4">
-        <a className="zoom green" href="work01.html"><img className="img-responsive" src="assets/img/portfolio/port05.jpg" alt="" /></a>
-        <p>EMPERORS</p>
-      </div>
-
-      <div className="col-lg-4">
-        <a className="zoom green" href="work01.html"><img className="img-responsive" src="assets/img/portfolio/port06.jpg" alt="" /></a>
-        <p>CHIEFS</p>
-      </div>
-    </div>
   </div>
     );
   }
