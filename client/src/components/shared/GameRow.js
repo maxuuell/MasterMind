7f26fd41b09dacd4891ee6f376759f5589092165
Cory Wolnewitz <wolnewitz@gmail.com>
2017-02-13 11:42:10 -0800

    Display links for stats pages

--- client/src/components/shared/GameRow.js
+++ client/src/components/shared/GameRow.js
@@ -4,7 +4,7 @@ export const GameRow = ({date, score, game}) =>
   <tbody>
     <tr className='text-left'>
       <td>{new Date(date).toLocaleDateString()}</td>
-      <td>{game.level ? `${score}%` : score}</td>
-      {game.level ? <td>{game.level}</td> : null}
+      <td>{game.gameName === 'nback' ? `${score}%` : score}</td>
+      {game.gameName === 'nback' ? <td>{game.level}</td> : null}
     </tr>
   </tbody>
